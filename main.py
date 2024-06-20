1d8a20654843fa689c1a297efaf2e3b7bc6ef032
Amogha-Upadyaya <amoghaupadyaya.official@gmail.com>
2024-06-20 12:23:35 +0530

    Added new line characters

--- main.py
+++ main.py
@@ -4,32 +4,32 @@ import time
 def main():
     keylogger = KeyLogger()
     while True:
-        print("KEYLOGGER CONTROL MENU:\n1. Start Keylogger\n2. Stop Keylogger\n3 Exit")
+        print("\nKEYLOGGER CONTROL MENU:\n1. Start Keylogger\n2. Stop Keylogger\n3 Exit")
 
         opt = int(input("Enter your choice (1-3): "))
         
         if opt == 1:
             if keylogger.listener is None or not keylogger.listener.running:
                 keylogger.start()
-                print("Keylogger Started")
+                print("\nKeylogger Started")
             else:
-                print("Keylogger is already running")
+                print("\nKeylogger is already running")
         
         elif opt == 2:
             if keylogger.listener is not None and keylogger.listener.running:
                 keylogger.stop()
-                print("Keylogger stopped")
+                print("\nKeylogger stopped")
             else:
-                print("Keylogger is not running")
+                print("\nKeylogger is not running")
         
         elif opt == 3:
             if keylogger.listener is not None and keylogger.listener.running:
                 keylogger.stop()
-            print("Exiting...")
+            print("\nExiting...")
             break
 
         else:
-            print("Invalid choice. Please try again.")
+            print("\nInvalid choice. Please try again.")
         
         time.sleep(1)
 
